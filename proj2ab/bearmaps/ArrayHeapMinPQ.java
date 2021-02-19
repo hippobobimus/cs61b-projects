78a178a88e3eaaf12291f4afb1d8448a072b7674
Rob Masters <rjcmasters@gmail.com>
2021-02-19 17:33:17 +0000

    proj2b complete except for timing tests

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -1,21 +1,119 @@
 package bearmaps;
 
 import java.util.ArrayList;
+import java.util.TreeMap;
 import java.util.NoSuchElementException;
 import java.lang.IllegalArgumentException;
 
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
-    private ArrayList<PriorityNode> items;
+    private ArrayList<PriorityNode> heap;
+    private TreeMap<T, PriorityNode> items;
     private int size;
 
     public ArrayHeapMinPQ() {
-        items = new ArrayList<PriorityNode>();
+        heap = new ArrayList<PriorityNode>();
         size = 0;
+
+        items = new TreeMap<T, PriorityNode>();
     }
+
+    /** Adds a new item to the PQ with associated priority. */
+    @Override
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException("Item already present.");
+        }
+        PriorityNode node = new PriorityNode(item, priority, size);
+        heap.add(node);
+        items.put(item, node);
+        size++;
+        swim(last());
+    }
+
+    /* Returns true if the PQ contains the given item. */
+    @Override
+    public boolean contains(T item) {
+        return items.containsKey(item);
+    }
+
+    /* Returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+    @Override
+    public T getSmallest() {
+        if (heap.isEmpty()) {
+            throw new NoSuchElementException("The PQ is empty.");
+        }
+        return root().getItem();
+    }
+
+    /* Removes and returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+    @Override
+    public T removeSmallest() {
+        if (heap.isEmpty()) {
+            throw new NoSuchElementException("The PQ is empty.");
+        }
+        T smallestItem = root().getItem();
+        replaceRootWithLast();
+        if (size() > 0) {
+            sink(root());
+        }
+        return smallestItem;
+    }
+
+    /* Returns the number of items in the PQ. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /* Changes the priority of the given item. Throws NoSuchElementException if the item 
+     * doesn't exist. */
+    @Override
+    public void changePriority(T item, double priority) {
+        PriorityNode node = items.get(item);
+        if (node == null) {
+            throw new NoSuchElementException("Item not found.");
+        }
+        node.setPriority(priority);
+        swim(node);
+        sink(node);
+    }
+
+    @Override
+    public String toString() {
+        int depth = ((int) (Math.log(size()) / Math.log(2)));
+        int level = 0;
+        int itemsUntilNext = 0;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < Math.pow(2, depth) - 1; j++) {
+                result.append("     ");
+            }
+            result.append(heap.get(i));
+            for (int j = 0; j < Math.pow(2, depth) - 1; j++) {
+                result.append("     ");
+            }
+            if (i == itemsUntilNext) {
+                result.append("\n");
+                level++;
+                itemsUntilNext += Math.pow(2, level);
+                depth--;
+            }
+        }
+        result.append("\n");
+        return result.toString();
+    }
+
+    /** HELPER METHODS  */
 
     /** Returns the root node. */
     private PriorityNode root() {
-        return items.get(0);
+        return heap.get(0);
+    }
+
+    /** Returns the last node. */
+    private PriorityNode last() {
+        return heap.get(size - 1);
     }
 
     /** Throws an exception if the key is not in the valid range. */
@@ -56,35 +154,52 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         return r;
     }
 
-    /** Replaces the node at k1 with the node at k2. */
-    private void replace(int k1, int k2) {
-        validateKey(k1);
-        validateKey(k2);
-        items.set(k1, items.get(k2));
-        items.remove(k2);
+    private void removeLast() {
+        PriorityNode l = last();
+        items.remove(l.getItem());
+        heap.remove(l.getKey());
         size--;
+    }
+
+    /** Replaces the root node with the last node. */
+    private void replaceRootWithLast() {
+        swap(root(), last());
+        removeLast();
     }
 
     /** Swaps the positions of two nodes. */
     private void swap(int k1, int k2) {
         validateKey(k1);
         validateKey(k2);
-        PriorityNode temp = items.get(k1);
-        items.set(k1, items.get(k2));
-        items.set(k2, temp);
+        if (k1 == k2) {
+            return;
+        }
+        PriorityNode node1 = heap.get(k1);
+        PriorityNode node2 = heap.get(k2);
+        heap.set(k1, node2);
+        node2.setKey(k1);
+        heap.set(k2, node1);
+        node1.setKey(k2);
     }
 
-    /** Lifts a node up to the appropriate position based on priority. */
+    private void swap(PriorityNode node1, PriorityNode node2) {
+        swap(node1.getKey(), node2.getKey());
+    }
+
     private void swim(int key) {
         validateKey(key);
         int p = parent(key);
-        if (items.get(key).compareTo(items.get(p)) < 0) {
+        if (heap.get(key).compareTo(heap.get(p)) < 0) {
             swap(key, p);
             swim(p);
         }
     }
 
-    /** Drops a node down to the appropriate position based on priority. */
+    /** Lifts a node up to the appropriate position based on priority. */
+    private void swim(PriorityNode node) {
+        swim(node.getKey());
+    }
+
     private void sink(int key) {
         validateKey(key);
         int lKey = leftChild(key);
@@ -97,77 +212,30 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         } else if (rKey < 0) {
             smallestChild = lKey;
         } else {
-            PriorityNode lNode = items.get(lKey);
-            PriorityNode rNode = items.get(rKey);
+            PriorityNode lNode = heap.get(lKey);
+            PriorityNode rNode = heap.get(rKey);
             smallestChild = lNode.compareTo(rNode) < 0 ? lKey : rKey;
         }
-        if (items.get(key).compareTo(items.get(smallestChild)) > 0) {
+        if (heap.get(key).compareTo(heap.get(smallestChild)) > 0) {
             swap(key, smallestChild);
             sink(smallestChild);
         }
     }
 
-    /** Returns the key of the last Node (bottom right). */
-    private int lastKey() {
-        return size() - 1;
-    }
-
-    /** Adds a new item to the PQ with associated priority. */
-    @Override
-    public void add(T item, double priority) {
-        items.add(new PriorityNode(item, priority));
-        size++;
-        swim(lastKey());
-    }
-
-    /* Returns true if the PQ contains the given item. */
-    @Override
-    public boolean contains(T item) {
-        // TODO
-        return false;
-    }
-
-    /* Returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
-    @Override
-    public T getSmallest() {
-        if (items.isEmpty()) {
-            throw new NoSuchElementException("The PQ is empty.");
-        }
-        return root().getItem();
-    }
-
-    /* Removes and returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
-    @Override
-    public T removeSmallest() {
-        if (items.isEmpty()) {
-            throw new NoSuchElementException("The PQ is empty.");
-        }
-        T smallestItem = root().getItem();
-        replace(0, lastKey());
-        sink(0);
-        return smallestItem;
-    }
-
-    /* Returns the number of items in the PQ. */
-    @Override
-    public int size() {
-        return size;
-    }
-
-    /* Changes the priority of the given item. Throws NoSuchElementException if the item 
-     * doesn't exist. */
-    @Override
-    public void changePriority(T item, double priority) {
-        // TODO
+    /** Drops a node down to the appropriate position based on priority. */
+    private void sink(PriorityNode node) {
+        sink(node.getKey());
     }
 
     private class PriorityNode implements Comparable<PriorityNode> {
         private T item;
         private double priority;
+        private int key;
 
-        public PriorityNode(T item, double priority) {
+        public PriorityNode(T item, double priority, int key) {
             this.item = item;
             this.priority = priority;
+            this.key = key;
         }
 
         T getItem() {
@@ -182,12 +250,25 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
             this.priority = priority;
         }
 
+        int getKey() {
+            return key;
+        }
+
+        void setKey(int key) {
+            this.key = key;
+        }
+
         @Override
         public int compareTo(PriorityNode other) {
             if (other == null) {
                 return -1;
             }
             return Double.compare(this.getPriority(), other.getPriority());
+        }
+
+        @Override
+        public String toString() {
+            return "[" + item + ", " + priority + "]";
         }
     }
 }
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,8 +1,9 @@
 package bearmaps;
 
 import java.util.Random;
+import java.util.Iterator;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ArrayHeapMinPQTest {
 
@@ -11,7 +12,48 @@ public class ArrayHeapMinPQTest {
     }
 
     @Test
-    public void testConstruction() {
+    public void testAddRemoveSizeSingleItem() {
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<String> nn = new NaiveMinPQ<>();
+
+        assertEquals(nn.size(), pq.size());
+
+        pq.add("hi", 1);
+        nn.add("hi", 1);
+
+        assertEquals(nn.size(), pq.size());
+        assertEquals(nn.getSmallest(), pq.getSmallest());
+        assertEquals(nn.size(), pq.size());
+        assertEquals(nn.removeSmallest(), pq.removeSmallest());
+        assertEquals(nn.size(), pq.size());
+    }
+
+    @Test
+    public void testAddRemoveSize() {
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<String> nn = new NaiveMinPQ<>();
+
+        long seed = 956;
+        Random rnd = new Random(seed);
+        Iterator<Integer> rndInts = rnd.ints(100, 0, 1_000_000).distinct().iterator();
+
+        for (int i = 0; i < 100; i++) {
+            //int priority = rnd.nextInt(100);
+            int priority = rndInts.next();
+            pq.add("hi" + i, priority);
+            nn.add("hi" + i, priority);
+        }
+
+        for (int i = 0; i < 100; i++) {
+            assertEquals(nn.size(), pq.size());
+            assertEquals(nn.getSmallest(), pq.getSmallest());
+            assertEquals(nn.removeSmallest(), pq.removeSmallest());
+            assertEquals(nn.size(), pq.size());
+        }
+    }
+
+    @Test
+    public void testChangePriority() {
         ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
         NaiveMinPQ<String> nn = new NaiveMinPQ<>();
 
@@ -19,17 +61,54 @@ public class ArrayHeapMinPQTest {
         Random rnd = new Random(seed);
 
         for (int i = 0; i < 100; i++) {
-            int priority = getRandomPriority(rnd);
+            int priority = rnd.nextInt(100);
             pq.add("hi" + i, priority);
             nn.add("hi" + i, priority);
         }
 
-        assertEquals(nn.size(), pq.size());
         assertEquals(nn.getSmallest(), pq.getSmallest());
 
-        assertEquals(nn.removeSmallest(), pq.removeSmallest());
-        assertEquals(nn.size(), pq.size());
+        int priority = rnd.nextInt(100);
+        pq.changePriority(pq.getSmallest(), priority);
+        nn.changePriority(nn.getSmallest(), priority);
+
         assertEquals(nn.getSmallest(), pq.getSmallest());
+    }
+
+    @Test
+    public void testExample() {
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<String> nn = new NaiveMinPQ<>();
+
+        String[] items = new String[]{"0th", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th"};
+        int[] priorities = new int[]{1, 9, 5, 4, 8, 2, 3, 0, 7, 6};
+
+        for (int i = 0; i < items.length; i++) {
+            pq.add(items[i], priorities[i]);
+            nn.add(items[i], priorities[i]);
+            //System.out.println("Inserting [" + items[i] + ", " + priorities[i] + "]...");
+            //System.out.println(pq);
+        }
+
+        System.out.println(pq);
+
+        assertEquals(10, pq.size());
+        assertEquals("7th", pq.getSmallest());
+
+        assertEquals("7th", pq.removeSmallest());
+        assertEquals(9, pq.size());
+        assertEquals("0th", pq.getSmallest());
+
+        assertEquals("0th", pq.removeSmallest());
+        assertEquals("5th", pq.removeSmallest());
+        assertEquals("6th", pq.removeSmallest());
+
+        assertTrue(pq.contains("3rd"));
+        assertFalse(pq.contains("11th"));
+
+        assertEquals("3rd", pq.getSmallest());
+        pq.changePriority("3rd", 8);
+        assertEquals("2nd", pq.getSmallest());
 
     }
 
